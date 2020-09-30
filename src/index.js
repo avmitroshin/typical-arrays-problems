e20e1ab69fe69210461bfe759bb54b6b7baf18fd
avmitroshin <2086007@mail.ru>
2020-09-30 23:19:02 +0700

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr_) {
+  if (!Array.isArray(arr_) || arr_.length < 1 )  return 0;
+
+  const arr = arr_.slice();
+  return (arr.sort((a, b) => a - b))[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr_) {
+  if (!Array.isArray(arr_) || arr_.length < 1 )  return 0;
+  
+  const arr = arr_.slice();
+  return (arr.sort((a, b) => b - a))[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr_) {
+  if (!Array.isArray(arr_) || arr_.length < 1 )  return 0;
+
+  let arr = arr_.slice();
+  
+  const result = arr.reduce( (total, value) => {
+    return total += value;
+  }, 0) / arr.length;
+
+  return result;
 }
